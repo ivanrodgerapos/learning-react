b5402f7b225be9eb6becfe8b15bd2de823b22533
ivanrodgerapos <ivanrodgerapos@gmail.com>
2020-08-11 00:01:09 +0800

    gh-pages

---
+++ build/precache-manifest.892fa3c1c576a5469049583138b01008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d24531885500b1bcb167eb9353031a7",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "1ac9eab706e8158625d0",
+    "url": "/learning-react/static/css/main.28c4b083.chunk.css"
+  },
+  {
+    "revision": "3deb1384f04073ccf2e1",
+    "url": "/learning-react/static/js/2.aebdfd40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learning-react/static/js/2.aebdfd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac9eab706e8158625d0",
+    "url": "/learning-react/static/js/main.7a72ff3f.chunk.js"
+  },
+  {
+    "revision": "3b126c0a5d33751f504e",
+    "url": "/learning-react/static/js/runtime-main.5ef7ebab.js"
+  }
+]);
